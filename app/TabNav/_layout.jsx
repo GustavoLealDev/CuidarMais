cec3014f26c2a466a24432f77f494ea149ee2f7a
GustavoLealDev <GustavoLealP@outlook.com>
2025-04-06 23:17:49 -0300

    Atualizando Navbar

--- app/TabNav/_layout.jsx
+++ app/TabNav/_layout.jsx
@@ -1,11 +1,39 @@
 import { View, Text } from 'react-native';
 import React from 'react';
 import { Tabs } from 'expo-router';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+
 
 export default function TabBar() {
   return (
-    <Tabs>
-        <Tabs.Screen name='index'></Tabs.Screen>
+    <Tabs screenOptions={{
+      headerShown: false
+    }}>
+        <Tabs.Screen name='index'
+          options={{
+            tabBarLabel:'Home',
+            tabBarIcon:({color,size})=>(
+              <FontAwesome name="home" size={size} color={color} />
+            )
+          }}
+        ></Tabs.Screen>  
+        <Tabs.Screen name='Add'
+        options={{
+          tabBarLabel:'Adicionar',
+          tabBarIcon:({color,size})=>(
+            <FontAwesome name="user-plus" size={size} color={color} />
+          )
+        }}>
+        </Tabs.Screen>
+        <Tabs.Screen name='Profile'
+        options={{
+          tabBarLabel:'Perfil',
+          tabBarIcon:({color,size})=>(
+            <FontAwesome name="user-circle" size={size} color={color} />
+          )
+        }}>
+
+        </Tabs.Screen>
     </Tabs>
   );
 }
