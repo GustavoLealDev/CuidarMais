00fe573a3a6ad7bbb5621c547ef18ec347a70d39
GustavoLealDev <GustavoLealP@outlook.com>
2025-04-10 14:56:32 -0300

    Colocando  segunda parte da autenticação

--- app/(TabNav)/_layout.jsx
+++ app/(TabNav)/_layout.jsx
@@ -1,10 +1,30 @@
 import { View, Text } from 'react-native';
 import React from 'react';
-import { Tabs } from 'expo-router';
+import { Tabs, useRouter } from 'expo-router';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../../configs/Firebase';
 
 
 export default function TabBar() {
+  
+  const router=useRouter();
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/auth.user
+      const uid = user.uid;
+      console.log(uid)
+      // ...
+    } else {
+      router?.push('/Login')
+      // User is signed out
+      // ...
+    }
+  })
+  
+  
   return (
     <Tabs screenOptions={{
       headerShown: false
---
+++ app/(TabNav)/index.jsx
@@ -0,0 +1,14 @@
+import { View, Text, Button } from 'react-native';
+import React from 'react';
+import { Redirect } from 'expo-router';
+import { signOut } from 'firebase/auth';
+import { auth } from '../../configs/Firebase';
+
+export default function Home() {
+  return (
+    <View>
+      <Text>Home</Text>
+      <Button title='Sair' onPress={()=> signOut()}/>
+    </View>
+  );
+}
