d3085b7f33f57319d450fbdebce3114184b222b7
GustavoLealDev <GustavoLealP@outlook.com>
2025-05-17 22:05:45 -0300

    atualizando notifications

--- CuidarMaisApp/utils/notifications.ts
+++ CuidarMaisApp/utils/notifications.ts
@@ -1,5 +1,6 @@
 import * as Notifications from "expo-notifications";
 import { Platform } from "react-native";
+import { Medication } from "./storage";
 
 Notifications.setNotificationHandler({
     handleNotification: async () => ({
@@ -41,5 +42,39 @@ export async function registerNotificationsAsync(): Promise<string|null> {
     } catch(error){
         console.error('Error ao obter token:', error);
         return null;
+    }
+}
+
+export async function scheduleMedication(medication: Medication): Promise<string| undefined> {
+    if(!medication.reminderEnable) return;
+
+    try{
+        for(const time of medication.times){
+            const [hours, minutes] = time.split(':').map(Number);
+            const today = new Date();
+            today.setHours(hours,minutes,0,0);
+
+            if(today < new Date()){
+                today.setDate(today.getDate() + 1)
+            }
+
+            const identifier = await Notifications.scheduleNotificationAsync({
+                content: {
+                    title: "lembrete de medicamentos",
+                     body: `Hora de tomar ${medication.name} (${medication.dosage})`,
+                    data: {medicationId: medication.id},
+                },
+                trigger: {
+                    hour: hours,
+                    minute: minutes,
+                    repeats: true,
+                },
+            });
+
+            return identifier;
+        }
+    } catch (error) {
+        console.error("Erro ao agendar lembrete de medicação", error);
+        return undefined;
     }
 }
